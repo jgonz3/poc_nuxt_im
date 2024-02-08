7bbd4fb85446ea0351627a480a3aec4aff7905dc
Kishar <kisharnat@gmail.com>
2024-02-08 12:58:37 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script >
+import Nav from '~/components/Nav.vue'
+</script>
+
+<template>
+  <div>
+    <Nav />
+    <UContainer>
+     <h2>Some packages for instant messaging</h2>
+  </UContainer>
+  </div>
+</template>
